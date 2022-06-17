d7bf63e5848a0cd4e9284d2bccc7aa874dc82f9a
Dan Hope <dplhope@gmail.com>
2022-06-17 10:05:39 +0100

    qatar itiration failing

--- javascript/medals.js
+++ javascript/medals.js
@@ -1,49 +1,39 @@
-    const first = 3 
-    const second = 2
-    const third = 1
+//i've created three loops which iterate over each podium finish and add points for 1st 2nd 3rd
+//  Only part of the test failing is that my fuction has Qatar on 2 when they should be on 3
+// Qatar is joint 1st in high jump. I need to alter function to take into account joint places.
 
 function createMedalTable(medals) {
-    // nunnumbers the first, second, and third podium strings
+    
     const firstPlace = medals.map(({podium}) => podium[0].split('').slice(2).join(''))
-      let firstPlaceCount = {}
+      let medalCount = {}
   	    for(let i=0; i<firstPlace.length; i++) {
-            if (firstPlaceCount[firstPlace[i]]) {
-                firstPlaceCount[firstPlace[i]] += 3
+            if (medalCount[firstPlace[i]]) {
+                medalCount[firstPlace[i]] += 3
         } else {
-                firstPlaceCount[firstPlace[i]] = 3
+                medalCount[firstPlace[i]] = 3
       }
     }
 
     const secondPlace = medals.map(({podium}) => podium[1].split('').slice(2).join(''))
-      let secondPlaceCount = {}
+    
         for(let i=0; i<secondPlace.length; i++) {
-            if (secondPlaceCount[secondPlace[i]]) {
-                secondPlaceCount[secondPlace[i]] += 2
+            if (medalCount[secondPlace[i]]) {
+                medalCount[secondPlace[i]] += 2
      } else {
-                 secondPlaceCount[secondPlace[i]] = 2
+                 medalCount[secondPlace[i]] = 2
      }
    }
     const thirdPlace = medals.map(({podium}) => podium[2].split('').slice(2).join(''))
-      let thirdPlaceCount = {}
+      
        for(let i=0; i<thirdPlace.length; i++) {
-         if (thirdPlaceCount[thirdPlace[i]]) {
-            thirdPlaceCount[thirdPlace[i]] += 1
+         if (medalCount[thirdPlace[i]]) {
+            medalCount[thirdPlace[i]] += 1
     } else {
-           thirdPlaceCount[thirdPlace[i]] = 1
+           medalCount[thirdPlace[i]] = 1
     }
   }
 
-    console.log(firstPlace)
-
-    // turns the podiums in key/values and adds points for first, second, and thrid 
-    // table is created but test is failing as it does not match to test
-    // What i need to do now is add duplicate scores together 
-
-    // const firstObj = Object.assign({}, ...firstPlace.map(key => ({[key]: first})))
-    // const secondObj = Object.assign({}, ...secondPlace.map(key => ({[key]: second})))
-    // const thirdObj = Object.assign({}, ...thirdPlace.map(key => ({[key]: third})))
-
-    const actualResult = {...firstPlaceCount, ...secondPlaceCount, ...thirdPlaceCount}
+const actualResult = medalCount
 
 console.log(actualResult)
 
